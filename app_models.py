a920d9d5bcc480f0b0152e1ea890c0b9c5dfcdce
Bhushan Songire <bhushanbsongire@gmail.com>
2024-11-17 04:39:18 +0530

    Refactor check_running_models and get_running_models functions to accept a temporary folder parameter and improve file handling

--- app_models.py
+++ app_models.py
@@ -2,28 +2,43 @@
 import os
 import subprocess
 
-FOLDER = "Threads"
-FILE = f"./{FOLDER}/tmp_running_models.txt"
+def check_running_models(temp_folder: str):
+    """Check the running models and save the output to a filename.
 
-if not os.path.exists(FOLDER):
-    os.makedirs(FOLDER)
+    Args:
+        temp_folder (str): Temporary folder to store the temp text file
 
-if os.path.exists(FILE):
-    os.remove(FILE)
+    Returns:
+        str: Filename of the text file
+    """
+
+    filename = f"{temp_folder}/tmp_running_models.txt"
+
+    # Clear the file if it already exists
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    # Run the command and save the output to the file
+    subprocess.run(["ollama", "ps"], stdout=open(filename, "w"))
+
+    return filename
 
 
-def check_running_models():
-    """Check the running models and save the output to a file."""
-    subprocess.run(["ollama", "ps"], stdout=open(FILE, "w"))
+def get_running_models(temp_folder: str):
+    """Get the running models from the file as a list.
+    
+    Args:
+        temp_folder (str): Temporary folder to store the temp text file
 
+    Returns:
+        list: List of running models
+    """
 
-def get_running_models():
-    """Get the running models from the file as a list."""
-    check_running_models()
+    filename = check_running_models(temp_folder=temp_folder)
     running_models = []
     ind = 0
 
-    with open(FILE, "r") as file:
+    with open(filename, "r") as file:
         for line_no, line_content in enumerate(file.readlines()):
             if line_no == 0:
                 ind = line_content.find("ID")
@@ -33,12 +48,8 @@ def get_running_models():
     return running_models
 
 
-def stop_running_models():
+def stop_running_models(running_models: list):
     """Stop the running models."""
-    check_running_models()
-    running_models = get_running_models()
-
     for model in running_models:
         subprocess.run(["ollama", "stop", model])
 
-    os.remove(FILE)
